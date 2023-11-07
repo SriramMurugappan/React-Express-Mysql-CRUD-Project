8a681f1ff6f196d15ffba663ff2ab9cbaa5627fa
SriramMurugappan <145545815+SriramMurugappan@users.noreply.github.com>
2023-11-07 11:58:55 +0530

    Update AboutUs.js

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -2,7 +2,7 @@ function AboutUs(props){
     return (<center><div><b>Contact Us at</b><br/>
         {props.companyName}<br/>
         {props.address}
-        <a href="mailto:Aboutus@gavstech.com?subject=About us">About Us</a>
+        <a href="mailto:aboutUs@gmail.com?subject=About us">About Us</a>
     </div></center>)
 }
 export default AboutUs;
